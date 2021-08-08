706fab5292b8179a9dcd994cf5e0177502f0f0ef
Kanha Agrawal <kanha.agr11@gmail.com>
2021-08-08 17:01:11 +0530

    hosted on firebase

--- src/components/connection/axios.js
+++ src/components/connection/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: "http://localhost:1312/",
+    baseURL: "https://myempmanager.herokuapp.com/",
 })
 
 export default instance;
