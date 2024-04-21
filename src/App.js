2881aa8df71b11a526d8324f96d3092029d33237
kanha-13 <kanha.agr11@gmail.com>
2024-04-22 00:00:00 +0530

    Refactor deleteTodo function

--- src/App.js
+++ src/App.js
@@ -281,3 +281,4 @@ export default App;
 /* Refactor deleteTodo function - 2024-12-31 22:34:02.800326 */
 /* Update Todo List placeholder text - 2024-12-31 22:34:03.144511 */
 /* Update Todo List placeholder text - 2024-12-31 22:34:03.222337 */
+/* Refactor deleteTodo function - 2024-12-31 22:34:03.815851 */
